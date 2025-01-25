c2a0ba0d2fdbe571b8768f4c4ede7cc38434f73f
Elayabarathi <elayabarathiedison@gmail.com>
2025-01-25 20:05:49 +0530

    refining code

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(player.move_up, "Up")
 
-
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    
     car_manager.create_car()
     car_manager.move_cars()
     
